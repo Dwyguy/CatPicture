cb30b4bb85419820c0024d74494a2f2df0d97234
Matthew Dwyer <dwyermw@muohio.edu>
2012-09-05 05:33:47 -0400

    Added picture to display
    Pacman in 3D!

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -12,7 +12,7 @@ using namespace std;
 class CatPictureApp : public AppBasic {
   public:
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseDown( MouseEvent event);	
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
@@ -27,11 +27,12 @@ class CatPictureApp : public AppBasic {
 	static const int appWidth = 800;
 	static const int surfaceSize = 1024;
 
-	void rectangle(uint8_t* surfaceArray, int x1, int y1, int x2, int y2);
+	void drawRectangle(uint8_t* surfaceArray, int x1, int y1, int x2, int y2, Color8u c);
 	void drawGradient(uint8_t* surfaceArray);
-	void drawCircle(uint8_t* surfaceArray, int centerX, int centerY, int radius);
+	void drawCircle(uint8_t* surfaceArray, int centerX, int centerY, int radius, Color8u c);
 	void tint(uint8_t* surfaceArray);
 	void blur(uint8_t* surfaceArray);
+	void drawPacMan(uint8_t* surfaceArray);
 };
 
 void CatPictureApp::prepareSettings(Settings* settings)
@@ -46,7 +47,7 @@ void CatPictureApp::setup() // When the program starts
 	uint8_t* surfaceArray = (*mySurface_).getData();
 }
 
-void CatPictureApp::rectangle(uint8_t* surfaceArray, int x1, int y1, int x2, int y2)
+void CatPictureApp::drawRectangle(uint8_t* surfaceArray, int x1, int y1, int x2, int y2, Color8u c)
 {
 	int startX = (x1 < x2) ? x1 : x2;
 	int endX = (x1 < x2) ? x2 : x1;
@@ -62,12 +63,10 @@ void CatPictureApp::rectangle(uint8_t* surfaceArray, int x1, int y1, int x2, int
 	if(endY >= appHeight)
 		endY = appHeight - 1;
 
-	Color8u c = Color8u(255, 0, 0);
-
 	// 100 means 100 pixels down
-	for(int y = 100; y <= endY; y++)
+	for(int y = startY; y <= endY; y++)
 	{
-		for(int x = 100; x <= endX; x++)
+		for(int x = startX; x <= endX; x++)
 		{
 			int ribbon = 3 * (x + y * surfaceSize);
 
@@ -78,13 +77,13 @@ void CatPictureApp::rectangle(uint8_t* surfaceArray, int x1, int y1, int x2, int
 	}
 }
 
-void CatPictureApp::drawCircle(uint8_t* surfaceArray, int centerX, int centerY, int radius)
+void CatPictureApp::drawCircle(uint8_t* surfaceArray, int centerX, int centerY, int radius, Color8u c)
 {
 	// Make sure the radius isn't negative
 	if(radius < 0)
 		return;
 
-	Color8u c = Color8u(0, 255, 0);
+	//Color8u c = Color8u(0, 255, 0);
 
 	// As a not for interesting effects later, if you get rid of the
 	// "- radius" in the initialization of the loop control variables
@@ -124,8 +123,8 @@ void CatPictureApp::drawGradient(uint8_t* surfaceArray)
 			int ribbon = 3 * (x + y * surfaceSize);
 			int special = (int)((256 * x) / appWidth);
 
-			surfaceArray[ribbon] = c.r + special;
-			surfaceArray[ribbon + 1] = c.g + special;
+			surfaceArray[ribbon] = c.r ;//+ special;
+			surfaceArray[ribbon + 1] = c.g;// + special;
 			surfaceArray[ribbon + 2] = c.b + special;		
 		}
 	}
@@ -133,7 +132,7 @@ void CatPictureApp::drawGradient(uint8_t* surfaceArray)
 
 void CatPictureApp::tint(uint8_t* surfaceArray)
 {
-	Color8u c = Color8u(0, 0, 200);
+	Color8u c = Color8u(255, 0, 0);
 
 	for(int y = 0; y < surfaceSize; y++)
 	{
@@ -161,14 +160,6 @@ void CatPictureApp::tint(uint8_t* surfaceArray)
 
 void CatPictureApp::blur(uint8_t* surfaceArray)
 {
-	//uint8_t* blurArray = [appWidth * appHeight * 3];
-	// Need a temporary image to blur, so we don't overwrite what we have
-	/*static uint8_t tempBlur[surfaceSize * surfaceSize * 3];
-	memcpy(tempBlur, surfaceArray, surfaceSize * surfaceSize * 3);
-
-	uint8_t kernel[9] = {4, 3, 4,
-						 4, 3, 4,
-						 4, 3, 4};*/
 
 	int ribbon, index;
 	uint8_t redVal = 0, greenVal = 0, blueVal = 0;
@@ -178,114 +169,61 @@ void CatPictureApp::blur(uint8_t* surfaceArray)
 	{
 		for(int x = 1; x <= appWidth - 1; x++)
 		{
-			index = 3 * (y + x * surfaceSize);
+			index = 3 * (x + y * surfaceSize);
 			total = 0;
 
 			for(int blurY = -1; blurY <= 1; blurY++)
 			{
 				for(int blurX = -1; blurX <= 1; blurX++)
 				{
-					total += surfaceArray[3 * (x + blurX + (y + blurY) * surfaceSize)];
+					redVal += surfaceArray[3 * (x + blurX + (y + blurY) * surfaceSize)];
+					greenVal += surfaceArray[3 * (x + blurX + (y + blurY) * surfaceSize) + 1];
+					blueVal += surfaceArray[3 * (x + blurX + (y + blurY) * surfaceSize) + 2];
 				}
 			}
 
-			surfaceArray[index] = total / 9;
-			surfaceArray[index + 1] = total / 9;
-			surfaceArray[index + 2] = total / 9;
+			surfaceArray[index] = redVal / 9;
+			surfaceArray[index + 1] = greenVal / 9;
+			surfaceArray[index + 2] = blueVal / 9;
 		}
 	}
 	
-	// Loops start at 1 so the edge is not taken into account
-	/*for(int y = 1; y <= appHeight - 1; y++)
-	{
-		for(int x = 1; x <= appWidth - 1; x++)
-		{
-			// Use appWidth instead of surfaceSize here because we only
-			// want to blur what is visible in the window.
-			//ribbon = 3 * (x + y * appWidth);
-			
-			redVal = 0;
-			greenVal = 0;
-			blueVal = 0;
-			
-			for(int blurY = y - 1; blurY <= y + 1; blurY++)
-			{
-				for(int blurX = x - 1; blurX <= x + 1; blurX++)
-				{
-					index = 3 * (blurX + (blurY) * surfaceSize);	
-
-					redVal += surfaceArray[index];
-					greenVal += surfaceArray[index + 1];
-					blueVal += surfaceArray[index + 2];
-
-					/*redVal += (tempBlur[index] >> n);
-					greenVal += (tempBlur[index + 1] >> n);
-					blueVal += (tempBlur[index + 2] >> n);*/
-
-					/*I tried to use something different than the bitwise operators
-					that Dr. Brinkman used because I didn't fully understand them,
-					but in not fully understanding them, I wasn't able to work around
-					them, or so I suspect.  Therefore, I am just using them and
-					will make a note to learn their exact function later.
-
-					tempBlur[index] += n;
-					tempBlur[index + 1] += n;
-					tempBlur[index + 2] += n;
-
-					redVal += n;
-					greenVal += n;
-					blueVal += n;*/
-				/*}
-			}				
-			
-			index = 3 * (x + y * surfaceSize);
-			surfaceArray[index] = redVal / 9;
-			surfaceArray[index + 1] = greenVal / 9;
-			surfaceArray[index + 2] = blueVal / 9;			
-		}
-	}*/
 }
 
-void CatPictureApp::mouseDown( MouseEvent event )
+void CatPictureApp::drawPacMan(uint8_t* surfaceArray)
 {
+	drawCircle(surfaceArray, 200, 100, 100, Color8u(255, 255, 0));// Body
+	drawCircle(surfaceArray, 180, 60, 12, Color8u(0, 0, 0)); // Left eye
+	drawCircle(surfaceArray, 250, 60, 12, Color8u(0, 0, 0)); // Right eye
+	drawRectangle(surfaceArray, 130, 110, 210, 155, Color8u(0, 0, 0)); // Mouth
+	drawCircle(surfaceArray, 300, 260, 30, Color8u(255, 255, 0));
+	drawCircle(surfaceArray, 400, 340, 40, Color8u(255, 255, 0));
+	drawCircle(surfaceArray, 520, 420, 50, Color8u(255, 255, 0));
+	drawCircle(surfaceArray, 640, 500, 60, Color8u(255, 255, 0));
+}
+
+void CatPictureApp::mouseDown( MouseEvent event)
+{
+	uint8_t* surfaceArray = (*mySurface_).getData();
+	tint(surfaceArray);
 }
 
 void CatPictureApp::update()
 {
-	
-	//int arrayLength = 3 * (*mySurface_).getWidth() * (*mySurface_).getHeight();
 	uint8_t* surfaceArray = (*mySurface_).getData();
 	drawGradient(surfaceArray);
-	rectangle(surfaceArray, 200, 300, 200, 300);
-	drawCircle(surfaceArray, 400, 400, 200);
-	//tint(surfaceArray);
-	blur(surfaceArray);
+
+	drawPacMan(surfaceArray);
+	//rectangle(surfaceArray, 200, 300, 200, 300);
+	//drawCircle(surfaceArray, 400, 400, 200);
 	
+	//blur(surfaceArray);
 
-	/*for(int x = 0; x < arrayLength; x++)
-	{		
-		surfaceArray[x] == 10;
-	}
-
-	(*myTexture_).update(*mySurface_, (*mySurface_).getBounds());
-	//*myTexture_ = gl::Texture(*mySurface_);
-
-	if(firstFrame)
-	{
-		firstFrame = false;
-	}*/
 }
 
 void CatPictureApp::draw()
 {
 	gl::draw(*mySurface_);
-	//gl::draw(*myTexture_);
-	//gl::Texture picture( loadImage( loadResource( RES_IMG) ) );
-	//gl::draw(picture);
-	//Color8u c = new Color8u(255, 0, 0);
-	//gl::draw(*myTexture_, Rectf(100, 50, 100, 50));
-	// clear out the window with black
-	//gl::clear( Color( brightness, greenValue, 0.0f ) ); 
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
